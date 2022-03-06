5cfc2e4d73eb9343cb8340b61a8ae6fd58cdaf9a
mancandoz <kozma7@hotmail.com>
2022-03-06 16:42:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ var express = require('express'),
     azureMobileApps = require('azure-mobile-apps');
 
 
-/*
+
 var app = express(),
     mobileApp = azureMobileApps();
 
+/*
 mobileApp.tables.add('Post'); // Create a table for 'Post' with default settings sds
 mobileApp.tables.add('Users');
 app.use(mobileApp);
