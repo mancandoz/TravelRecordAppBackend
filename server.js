f2139f6b2db8d900ffcb20d9925a8c0b44b6f980
mancandoz <kozma7@hotmail.com>
2022-03-06 14:31:44 +0300

    Mobile appp iptaş,

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var app = require('express')(); // Create an instance of an Express app
 
-var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
+//var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
-mobileApp.tables.add('Post'); // Create a table for 'Post' with default settings sds
-mobileApp.tables.add('Users');
-app.use(mobileApp);
-app.listen(process.env.PORT || 3000);
+//mobileApp.tables.add('Post'); // Create a table for 'Post' with default settings sds
+//mobileApp.tables.add('Users');
+//app.use(mobileApp);
+//app.listen(process.env.PORT || 3000);
+app.get('/', function (req, res) {
+res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+console.log('Example app listening on port 3000!')
+})
