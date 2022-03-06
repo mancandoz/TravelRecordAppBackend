be2ea2610b861b77c672535633f4a3011496ff9b
mancandoz <kozma7@hotmail.com>
2022-03-06 14:57:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,11 @@
-//var app = require('express')(); // Create an instance of an Express app
-
-//var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
-
-//mobileApp.tables.add('Post'); // Create a table for 'Post' with default settings sds
-//mobileApp.tables.add('Users');
-//app.use(mobileApp);
-//app.listen(process.env.PORT || 3000);
-
-
-
-const express = require('express') // npm install express
+const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
-// Send text
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', function (req, res) {
+res.send('Hello World!')
 })
 
-// Send HTML File
-app.get('/', (req, res) => {
-  res.sendFile('./index.html')
-})
-
-app.listen(port, () => {
-  console.log(`Your app is listening to port ${port}`)
+app.listen(port, function () {
+console.log('Example app listening on port 3000!')
 })
