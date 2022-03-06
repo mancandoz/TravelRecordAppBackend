013a7ae179339dcefa4c352bd585dcb63bc41a86
mancandoz <kozma7@hotmail.com>
2022-03-06 15:26:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+var express = require('express')
+var app = express()
+var port = process.env.PORT || 3000
 
 app.get('/', function (req, res) {
 res.send('Hello World!')
