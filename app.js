1445ab1be171e12a6e00dce8fbe4bbb97483404a
mancandoz <kozma7@hotmail.com>
2022-03-06 15:28:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // Copyright (c) 2015 Microsoft Corporation. All rights reserved.
 // ----------------------------------------------------------------------------
 
- use strict
+use strict
 
 // This is a base-level Azure Mobile App SDK
 var express = require('express'),
@@ -17,17 +17,13 @@ var app = express();
 // Configuration of the Azure Mobile Apps can be done via an object, the
 // environment or an auxiliary file.  For more information, see
 // http://azure.github.io/azure-mobile-apps-node/global.html#configuration
-
-/* Erim Bu yorumu sil
 var mobile = azureMobileApps({
     // Explicitly enable the Azure Mobile Apps home page
     homePage: true
 });
-Erim bu yorumu sil
-*/
-// Import the files from the tables directory to configure the /tables endpoint
 
-//erim bu da silinecek mobile.tables.import('./tables');
+// Import the files from the tables directory to configure the /tables endpoint
+mobile.tables.import('./tables');
 
 // Import the files from the api directory to configure the /api endpoint
 //! Un-comment if api folder exists
@@ -36,17 +32,8 @@ Erim bu yorumu sil
 // Initialize the database before listening for incoming requests
 // The tables.initialize() method does the initialization asynchronously
 // and returns a Promise.
-
-app.get('/', function (req, res) {
-res.send('Hello World!')
-})
-
-app.listen(3000, function () {
-console.log('Example app listening on port 3000!')
-})
-
-// yorumu sil mobile.tables.initialize()
- // yorumu sil    .then(function () {
-      // yorumu sil   app.use(mobile);    // Register the Azure Mobile Apps middleware
-     // yorumu sil    app.listen(process.env.PORT || 3000);   // Listen for requests
-   // yorumu sil  });
+mobile.tables.initialize()
+    .then(function () {
+        app.use(mobile);    // Register the Azure Mobile Apps middleware
+        app.listen(process.env.PORT || 3000);   // Listen for requests
+    });
