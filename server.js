1db6a5a3f6cea96cc7f62b59ebe498add23973ce
mancandoz <kozma7@hotmail.com>
2022-03-06 15:30:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ app.get('/', function (req, res) {
 res.send('Hello World!')
 })
 
+var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
+
+mobileApp.tables.add('Post'); // Create a table for 'Post' with default settings sds
+mobileApp.tables.add('Users');
+app.use(mobileApp);
+// app.listen(process.env.PORT || 3000);
+
+
+
 app.listen(port, function () {
 console.log('Example app listening on port 3000!')
 })
+
+
