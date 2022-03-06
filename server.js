c6a4a0fa568bbd7d7b3f246e007e8f2bd7e8248c
mancandoz <kozma7@hotmail.com>
2022-03-06 16:56:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,3 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
 
-app.get('/', function (req, res) {
-res.send('Hello World!')
-})
-
-app.listen(port, function () {
-console.log('Example app listening on port 3000!')
-})
+var express = require('express'),
+    azureMobileApps = require('azure-mobile-apps');
