d42125066b84d213712b800e33510b886c705e7a
mancandoz <kozma7@hotmail.com>
2022-03-06 18:31:55 +0300

    Update users.js

--- tables/users.js
+++ tables/users.js
@@ -5,19 +5,7 @@ var azureMobileApps = require('azure-mobile-apps');
 
 var table = azureMobileApps.table();
 
-table.columns = {
-    "text": "string",
-    "complete": "boolean"
-};
-table.seed = [
-    { text: 'Example 1', complete: false },
-    { text: 'Example 2', complete: true }
-];
 
-// Turn off the dynamic schema.
-table.dynamicSchema = false
-
-/*
 table.read(function (context) {
     return context.execute();
 });
@@ -33,5 +21,5 @@ table.update(function (context) {
 table.delete(function (context) {
     return context.execute();
 });
-*/
-module.exports = table;
+
+
